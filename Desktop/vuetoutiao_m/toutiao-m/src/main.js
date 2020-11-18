573a966f1659e6e7f4c734f11f900b882a82a63e
小白兔不白 <8268622+the-rabbit-is-not-white@user.noreply.gitee.com>
2020-11-18 11:48:02 +0800

    引入vant组件库

--- Desktop/vuetoutiao_m/toutiao-m/src/main.js
+++ Desktop/vuetoutiao_m/toutiao-m/src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
 
 // 加载全局样式
 import './styles/index.css'
 
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
